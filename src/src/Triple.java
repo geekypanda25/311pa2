7cf09f39c05a41f8a8c29c5bcdab206692e0f1d1
Benjamin Ferreira <ferreira@iastate.edu>
2019-04-17 23:21:00 -0500

    Added class to represent a Triple object, including getters for each field.

---
+++ src/src/Triple.java
@@ -0,0 +1,28 @@
+public class Triple implements Comparable<Triple> {
+
+    private ComputerNode Ci, Cj;
+    private int tk;
+
+    public Triple(ComputerNode Ci, ComputerNode Cj, int tk) {
+        this.Ci = Ci;
+        this.Cj = Cj;
+        this.tk = tk;
+    }
+
+    public ComputerNode getCi() {
+        return Ci;
+    }
+
+    public ComputerNode getCj() {
+        return Cj;
+    }
+
+    public int getTk() {
+        return tk;
+    }
+
+    @Override
+    public int compareTo(Triple o) {
+        return 0; //TODO
+    }
+}    
