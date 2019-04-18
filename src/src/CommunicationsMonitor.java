ad837e1b5e0c88ac3e686a79f8087847e348eb09
John <Schnoebelen>
2019-04-17 22:50:44 -0500

    addCommunication(int c1, int c2, int timestamp)

--- src/src/CommunicationsMonitor.java
+++ src/src/CommunicationsMonitor.java
@@ -1,6 +1,14 @@
 public class CommunicationsMonitor {
 
     public CommunicationsMonitor(){
-        
+
+    }
+
+    /* Takes as input two
+    integers c1, c2, and a timestamp. This triple represents the fact that the computers with
+    IDs c1 and c2 have communicated at the given timestamp. This method should run in
+    O(1) time. Any invocation of this method after createGraph() is called will be ignored  */
+    public void addCommunication(int c1, int c2, int timestamp){
+
     }
 }
