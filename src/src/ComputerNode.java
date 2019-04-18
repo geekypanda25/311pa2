3f74ab04e2415548ecbc9a6fd73d45479b65662e
Benjamin Ferreira <ferreira@iastate.edu>
2019-04-17 22:48:03 -0500

    Added required methods for ComputerNode class

--- src/src/ComputerNode.java
+++ src/src/ComputerNode.java
@@ -1,2 +1,19 @@
+import java.util.List;
+
 public class ComputerNode {
+
+
+    public int getID() {
+        return -1; //TODO
+    }
+
+
+    public int getTimestamp() {
+        return -1; //TODO
+    }
+
+
+    public List<ComputerNode> getOutNeighbors() {
+        return null; //TODO
+    }
 }
