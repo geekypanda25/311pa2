cc158e63166af44400094fca4feed1f7648652be
Benjamin Ferreira <ferreira@iastate.edu>
2019-04-17 22:52:37 -0500

    blah blah blah

--- src/src/CommunicationsMonitor.java
+++ src/src/CommunicationsMonitor.java
@@ -4,10 +4,12 @@ public class CommunicationsMonitor {
 
     }
 
-    /* Takes as input two
-    integers c1, c2, and a timestamp. This triple represents the fact that the computers with
-    IDs c1 and c2 have communicated at the given timestamp. This method should run in
-    O(1) time. Any invocation of this method after createGraph() is called will be ignored  */
+    /**
+     * Takes as input two
+     *     integers c1, c2, and a timestamp. This triple represents the fact that the computers with
+     *     IDs c1 and c2 have communicated at the given timestamp. This method should run in
+     *     O(1) time. Any invocation of this method after createGraph() is called will be ignored
+     **/
     public void addCommunication(int c1, int c2, int timestamp){
 
     }
